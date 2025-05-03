f2d6cff98fd2ee46e4c6f66fcc6563924a5cdc49
seongho00 <tjdwns000927@gmail.com>
2025-05-03 18:59:11 +0900

    저주의 숫자 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,36 +7,30 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    public int solution(String my_string) {
+    public int solution(int n) {
         int answer = 0;
-        my_string = my_string.toLowerCase();
-        List<String> words = new ArrayList<>();
-        for (int i = 0; i < 26; i++) {
-            words.add(String.valueOf((char) (i + 97)));
-        }
-        for (int i = 0; i < words.size(); i++) {
-            my_string = my_string.replaceAll(words.get(i), " ");
-        }
-        List<String> numbers = Arrays.asList(my_string.split(" "));
-
-        for (int i = 0; i < numbers.size(); i++) {
-            try {
-                answer += Integer.parseInt(numbers.get(i));
-
-            } catch (Exception e) {
-                
+        int i = 0;
+        while (i < n) {
+            if (answer % 3 == 0) {
+                answer += 1;
+                continue;
+            } else if (String.valueOf(answer).contains("3")) {
+                answer += 1;
+                continue;
             }
+            answer += 1;
+            i++;
         }
 
-
-        return answer;
+        return answer - 1;
     }
 }
+
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("aAb1B2cC34oOp"));
+        System.out.println(solution.solution(19));
     }
 }
