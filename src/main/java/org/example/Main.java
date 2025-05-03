154908c92a725ff1a7288b7481f44bdf22c63c56
seongho00 <tjdwns000927@gmail.com>
2025-05-03 21:06:57 +0900

    외계인 사전

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,59 +6,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 class Solution {
-    public int solution(int[][] board) {
-        int answer = 0;
-        int rowMinus;
-        int colMinus;
-        int rowPlus;
-        int colPlus;
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[row].length; col++) {
-                if (board[row][col] == 1) {
-                    if (row - 1 < 0) {
-                        rowMinus = 0;
-                    } else {
-                        rowMinus = row - 1;
-                    }
-                    if (row + 1 > board.length - 1) {
-                        rowPlus = board.length - 1;
-                    } else {
-                        rowPlus = row + 1;
-                    }
-
-                    if (col - 1 < 0) {
-                        colMinus = 0;
-                    } else {
-                        colMinus = col - 1;
-                    }
-                    if (col + 1 > board[row].length - 1) {
-                        colPlus = board.length - 1;
-                    } else {
-                        colPlus = col + 1;
-                    }
-
-
-                    board[row][colPlus] = board[row][colPlus] == 1 ? 1 : 2;
-                    board[row][colMinus] = board[row][colMinus] == 1 ? 1 : 2;
-                    board[rowMinus][col] = board[rowMinus][col] == 1 ? 1 : 2;
-                    board[rowMinus][colMinus] = board[rowMinus][colMinus] == 1 ? 1 : 2;
-                    board[rowMinus][colPlus] = board[rowMinus][colPlus] == 1 ? 1 : 2;
-                    board[rowPlus][col] = board[rowPlus][col] == 1 ? 1 : 2;
-                    board[rowPlus][colMinus] = board[rowPlus][colMinus] == 1 ? 1 : 2;
-                    board[rowPlus][colPlus] = board[rowPlus][colPlus] == 1 ? 1 : 2;
-                }
+    public int solution(String[] spell, String[] dic) {
+        for (int i = 0; i < dic.length; i++) {
+            for (int j = 0; j < spell.length; j++) {
+                dic[i] = dic[i].replaceAll(spell[j], String.valueOf(j));
             }
-        }
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[row].length; col++) {
-                if (board[row][col] == 0) {
+            int answer = 0;
+
+            for (int j = 0; j < spell.length; j++) {
+                if (dic[i].contains(String.valueOf(j))) {
                     answer++;
                 }
+
+                if (answer == spell.length) {
+                    return 1;
+                }
             }
         }
-        return answer;
-
+        return 2;
     }
 }
 
@@ -67,6 +34,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[][]{{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}}));
+        System.out.println(solution.solution(new String[]{"s", "o", "m", "d"}, new String[]{"moos", "dzx", "smm", "sunmmo", "som"}));
     }
 }
