d9814305669edd07cebd278302f0eafe6dfe8f27
seongho00 <tjdwns000927@gmail.com>
2025-05-03 19:24:15 +0900

    평행

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,22 +7,38 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
+    public int solution(int[][] dots) {
         int i = 0;
-        while (i < n) {
-            if (answer % 3 == 0) {
-                answer += 1;
-                continue;
-            } else if (String.valueOf(answer).contains("3")) {
-                answer += 1;
-                continue;
+        int first_dot = 0;
+        int second_dot = 1;
+        int third_dot = 2;
+        int fourth_dot = 3;
+        while (i < 3) {
+            if (second_dot == 2) {
+                third_dot = 1;
             }
-            answer += 1;
+            if (second_dot == 3) {
+                fourth_dot = 1;
+            }
+            int fir_inclination_x = dots[first_dot][0] - dots[second_dot][0];
+            int fir_inclination_y = dots[first_dot][1] - dots[second_dot][1];
+            double fir_inclination = (double) fir_inclination_x / fir_inclination_y;
+
+            int sec_inclination_x = dots[third_dot][0] - dots[fourth_dot][0];
+            int sec_inclination_y = dots[third_dot][1] - dots[fourth_dot][1];
+
+            double sec_inclination = (double) sec_inclination_x / sec_inclination_y;
+
+            if (fir_inclination == sec_inclination) {
+                return 1;
+            }
+
+            second_dot++;
             i++;
         }
 
-        return answer - 1;
+
+        return 0;
     }
 }
 
@@ -31,6 +47,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(19));
+        System.out.println(solution.solution(new int[][]{{1, 4}, {9, 2}, {3, 8}, {11, 6}}));
     }
 }
