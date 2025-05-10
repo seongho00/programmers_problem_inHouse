7efb358900694db1edb223b7c9dbb2bdd1696c99
seongho00 <tjdwns000927@gmail.com>
2025-05-10 16:38:21 +0900

    문자열의 앞의 n글자

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,9 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(String[] intStrs, int k, int s, int l) {
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < intStrs.length; i++) {
-            int a = Integer.parseInt(String.valueOf(intStrs[i]).substring(s, s + l));
-            if (a > k) {
-                answer.add(a);
-            }
-        }
-        return answer;
+    public String solution(String my_string, int n) {
+
+        return my_string.substring(0, n);
     }
 }
 
@@ -21,6 +15,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new String[]{"0123456789", "9876543210", "9999999999999" }, 50000, 5, 5));
+        System.out.println(solution.solution("ProgrammerS123", 11));
     }
 }
