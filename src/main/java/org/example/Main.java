c72b10d1c773df9386bcf1e4f2e9ab26af8f7846
seongho00 <tjdwns000927@gmail.com>
2025-05-10 16:28:32 +0900

    접미사인지 확인하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,18 @@
 package org.example;
 
 
-import java.util.Collections;
 import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Solution {
-    public List<String> solution(String my_string) {
-        List<String> answer = new ArrayList<>();
-        for (int i = my_string.length(); i > 0; i--) {
-            answer.add(my_string.substring(i - 1));
+    public int solution(String my_string, String is_suffix) {
+        if (my_string.endsWith(is_suffix)) {
+            return 1;
         }
-        Collections.sort(answer);
-        return answer;
+        return 0;
     }
 }
 
@@ -22,6 +20,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("banana"));
+        System.out.println(solution.solution("banana", "ana"));
     }
 }
