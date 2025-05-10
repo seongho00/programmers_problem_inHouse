b59f60450b5ddbc7be3b1af73efd429f023b4358
seongho00 <tjdwns000927@gmail.com>
2025-05-10 15:17:28 +0900

    종이 자르기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,20 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public int solution(String A, String B) {
-        int answer = 0;
-        if (A.equals(B)) {
-            return 0;
-        }
-        for (int i = 0; i < A.length(); i++) {
-            char last_char = A.charAt(A.length() - 1);
-            A = last_char + A.substring(0, A.length() - 1);
-            answer++;
-            if (A.equals(B)) {
-                return answer;
-            }
-        }
-        return -1;
+    public int solution(int M, int N) {
+        return M * N - 1;
     }
 }
 
@@ -29,6 +17,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("apple", "tata"));
+        System.out.println(solution.solution(3, 4));
     }
 }
