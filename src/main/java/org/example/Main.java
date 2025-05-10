2d95178c1e152b466910371c4eacd7c537278345
seongho00 <tjdwns000927@gmail.com>
2025-05-10 14:37:58 +0900

    이진수 더하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,31 +8,11 @@ import java.util.Collections;
 import java.util.List;
 
 class Solution {
-    public List<Integer> solution(int[][] score) {
-        List<Double> AVG_sort = new ArrayList<>();
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < score.length; i++) {
-            int scoreSum = 0;
+    public String solution(String bin1, String bin2) {
 
-            for (int j = 0; j < score[i].length; j++) {
-                scoreSum += score[i][j];
-            }
-            AVG_sort.add((double) scoreSum / (double) score[0].length);
-        }
-
-        List<Double> AVG = new ArrayList<>(AVG_sort);
-        Collections.sort(AVG_sort, Collections.reverseOrder());
-        for (Double avg : AVG) {
-            int avg_int = (int) (avg * 10);
-            for (int j = 0; j < AVG_sort.size(); j++) {
-                if ((int) (AVG_sort.get(j) * 10) == avg_int) {
-                    answer.add(j + 1);
-                    break;
-                }
-            }
-        }
-
-        return answer;
+        int bin1_10 = Integer.parseInt(bin1, 2);
+        int bin2_10 = Integer.parseInt(bin2, 2);
+        return Integer.toBinaryString(bin1_10 + bin2_10);
     }
 }
 
@@ -40,6 +20,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[][]{{1, 3}, {3, 1}, {2, 3}, {3, 2}, {1, 2}, {1, 1}}));
+        System.out.println(solution.solution("1001", "1111"));
     }
 }
