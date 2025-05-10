3e536e90da8bc10826e2c50c714ee37cb3289ef1
seongho00 <tjdwns000927@gmail.com>
2025-05-10 16:39:19 +0900

    접두사인지 확인하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,11 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 class Solution {
-    public String solution(String my_string, int n) {
-
-        return my_string.substring(0, n);
+    public int solution(String my_string, String is_prefix) {
+        if (my_string.startsWith(is_prefix)) {
+            return 1;
+        }
+        return 0;
     }
 }
 
@@ -15,6 +17,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("ProgrammerS123", 11));
+        System.out.println(solution.solution("banana", "ban"));
     }
 }
