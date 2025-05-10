8fba8ebf17f1c5627231c90991feea10856b6eec
seongho00 <tjdwns000927@gmail.com>
2025-05-10 16:36:50 +0900

    배열 만들기 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,16 @@ package org.example;
 
 
 import java.sql.SQLOutput;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    public String solution(String[] my_strings, int[][] parts) {
-        String answer = "";
-        for (int i = 0; i < my_strings.length; i++) {
-            answer += my_strings[i].substring(parts[i][0], parts[i][1] + 1);
+    public List<Integer> solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < intStrs.length; i++) {
+            int a = Integer.parseInt(String.valueOf(intStrs[i]).substring(s, s + l));
+            if (a > k) {
+                answer.add(a);
+            }
         }
         return answer;
     }
@@ -21,6 +21,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new String[]{"progressive", "hamburger", "hammer", "ahocorasick" }, new int[][]{{0, 4}, {1, 2}, {3, 5}, {7, 7}}));
+        System.out.println(solution.solution(new String[]{"0123456789", "9876543210", "9999999999999" }, 50000, 5, 5));
     }
 }
