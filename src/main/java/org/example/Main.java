10b5b7732526aff6c65aa6f1d7b06f4dc44410a9
seongho00 <tjdwns000927@gmail.com>
2025-05-11 17:13:17 +0900

    첫 번째로 나오는 음수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,32 +5,13 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int n, int[] slicer, int[] num_list) {
-        List<Integer> answer = new ArrayList<>();
-        int start = 0;
-        int end = num_list.length - 1;
-        int interval = 1;
-        if (n == 1) {
-            end = slicer[1];
-
-        } else if (n == 2) {
-            start = slicer[0];
-
-        } else if (n == 3) {
-            start = slicer[0];
-            end = slicer[1];
-
-        } else {
-            start = slicer[0];
-            end = slicer[1];
-            interval = slicer[2];
-
+    public int solution(int[] num_list) {
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] < 0) {
+                return i;
+            }
         }
-        for (int i = start; i <= end; i += interval) {
-            answer.add(num_list[i]);
-        }
-
-        return answer;
+        return -1;
     }
 }
 
@@ -38,6 +19,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(2, new int[]{1, 5, 2}, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}));
+        System.out.println(solution.solution(new int[]{12, 4, 15, 46, 38, -2, 15}));
     }
 }
