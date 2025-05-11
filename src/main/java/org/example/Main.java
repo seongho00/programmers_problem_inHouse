54e2058216b7ee6f2f14398e4ec9c849eefac429
seongho00 <tjdwns000927@gmail.com>
2025-05-11 17:20:21 +0900

    2의 영역

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,28 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int[] arr, int[][] intervals) {
+    public List<Integer> solution(int[] arr) {
         List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < intervals.length; i++) {
-            for (int j = intervals[i][0]; j <= intervals[i][1]; j++) {
-                answer.add(arr[j]);
+        int start_index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                start_index = i;
+                break;
             }
+        }
+        if (start_index == -1) {
+            answer.add(-1);
+            return answer;
+        }
+        int end_index = -1;
+        for (int i = arr.length - 1; i > 0; i--) {
+            if (arr[i] == 2) {
+                end_index = i;
+                break;
+            }
+        }
+        for (int i = start_index; i <= end_index; i++) {
+            answer.add(arr[i]);
         }
         return answer;
     }
@@ -20,6 +36,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{1, 2, 3, 4, 5}, new int[][]{{1, 3}, {0, 4}}));
+        System.out.println(solution.solution(new int[]{1, 2, 1, 4, 5, 2, 9}));
     }
 }
