17c31dba1334c5e95aa6caf2048b083d9421b604
seongho00 <tjdwns000927@gmail.com>
2025-05-11 16:39:35 +0900

    글자 지우기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,13 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 class Solution {
-    public String solution(int q, int r, String code) {
-        String answer = "";
-        for (int i = 0; i < code.length(); i++) {
-            if (i % q == r) {
-                answer += code.charAt(i);
-            }
+    public String solution(String my_string, int[] indices) {
+        List<String> list = new ArrayList<>(Arrays.asList(my_string.split("")));
+
+        for (int i = 0; i < indices.length; i++) {
+            list.set(indices[i], "");
         }
-        return answer;
+        return String.join("", list);
     }
 }
 
@@ -20,6 +19,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(3, 1, "qjnwezgrpirldywt"));
+        System.out.println(solution.solution("apporoograpemmemprs", new int[]{1, 16, 6, 15, 0, 10, 11, 3}));
     }
 }
