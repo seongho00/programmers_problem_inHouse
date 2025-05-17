9afccc0c76c98ce139d82d400beba89c4e42ee98
seongho00 <tjdwns000927@gmail.com>
2025-05-17 15:52:25 +0900

    정수 부분

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,25 +5,8 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 class Solution {
-    public int solution(int[] rank, boolean[] attendance) {
-        int answer = 0;
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < rank.length; i++) {
-            list.add(rank[i]);
-        }
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < rank.length; i++) {
-            if (attendance[i]) {
-                map.put(rank[i], list.indexOf(rank[i]));
-            }
-        }
-        List<Integer> result_list = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            result_list.add(map.get(Collections.min(map.keySet())));
-            map.remove(Collections.min(map.keySet()));
-        }
-        answer = result_list.get(0) * 10000 + result_list.get(1) * 100 + result_list.get(2);
-        return answer;
+    public int solution(double flo) {
+        return (int) flo;
     }
 }
 
@@ -31,6 +14,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{3, 7, 2, 5, 4, 6, 1}, new boolean[]{false, true, true, true, true, false, false}));
+        System.out.println(solution.solution(1.42));
     }
 }
