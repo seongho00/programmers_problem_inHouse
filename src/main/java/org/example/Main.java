281cee37dbc22565261c0586e9ea886135c2c47d
seongho00 <tjdwns000927@gmail.com>
2025-05-17 15:53:31 +0900

    문자열 정수의 합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,13 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 class Solution {
-    public int solution(double flo) {
-        return (int) flo;
+    public int solution(String num_str) {
+        int answer = 0;
+        List<String> list = new ArrayList<>(Arrays.asList(num_str.split("")));
+        for (int i = 0; i < list.size(); i++) {
+            answer += Integer.parseInt(list.get(i));
+        }
+        return answer;
     }
 }
 
@@ -14,6 +19,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(1.42));
+        System.out.println(solution.solution("123456789"));
     }
 }
