71a9fc3591125ffae481b2d9b3a5425578d671b2
seongho00 <tjdwns000927@gmail.com>
2025-05-17 16:01:23 +0900

    문자열 묶기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,36 @@
 package org.example;
 
 
-import java.sql.SQLOutput;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Solution {
-    public int solution(String n_str) {
-        return Integer.parseInt(n_str);
+    public int solution(String[] strArr) {
+        int answer = 0;
+        List<Integer> length = new ArrayList<>();
+        for (int i = 0; i < strArr.length; i++) {
+            length.add(strArr[i].length());
+        }
+        length = length.stream().distinct().collect(Collectors.toList());
+        Collections.sort(length);
+
+        for (int i = 0; i < length.size(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < strArr.length; j++) {
+
+                if (length.get(i) == strArr[j].length()) {
+                    count++;
+                }
+            }
+            if (answer < count) {
+                answer = count;
+            }
+        }
+
+        return answer;
     }
 }
 
@@ -14,6 +38,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution("123456789"));
+        System.out.println(solution.solution(new String[]{"a", "bc", "d", "efg", "hi"}));
     }
 }
