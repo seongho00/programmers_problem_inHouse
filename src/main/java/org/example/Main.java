f61637334b064ef4a2f5ca618ab992a706fd298a
seongho00 <tjdwns000927@gmail.com>
2025-05-17 15:26:01 +0900

    배열의 길이에 따라 다른 연산하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,30 +5,19 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 class Solution {
-    public int solution(int[] arr1, int[] arr2) {
-        if (arr1.length != arr2.length) {
-            if (arr1.length > arr2.length) {
-                return 1;
-            }
-            return -1;
-        }
-        int arr1sum = 0;
-        int arr2sum = 0;
-        for (int i = 0; i < arr1.length; i++) {
-            arr1sum += arr1[i];
-        }
-        for (int i = 0; i < arr2.length; i++) {
-            arr2sum += arr2[i];
-        }
-        if (arr1sum == arr2sum) {
-            return 0;
-        } else {
-            if (arr1sum > arr2sum) {
-                return 1;
-            }
-            return -1;
-        }
+    public int[] solution(int[] arr, int n) {
 
+
+        if (arr.length % 2 == 0) {
+            for (int i = 1; i < arr.length; i += 2) {
+                arr[i] = arr[i] + n;
+            }
+        } else {
+            for (int i = 0; i < arr.length; i += 2) {
+                arr[i] = arr[i] + n;
+            }
+        }
+        return arr;
     }
 }
 
@@ -36,6 +25,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{49, 13}, new int[]{70, 11, 2}));
+        System.out.println(solution.solution(new int[]{49, 12, 100, 276, 33}, 27));
     }
 }
