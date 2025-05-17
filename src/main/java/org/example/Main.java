3ad153195ab40283e4c1e53058627f25ec3e31b4
seongho00 <tjdwns000927@gmail.com>
2025-05-17 15:19:15 +0900

    배열 비교하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,20 +5,30 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int[] arr, int[] query) {
-        List<Integer> answer = new ArrayList<>();
-        for (int i : arr) {
-            answer.add(i);
-        }
-        for (int i = 0; i < query.length; i++) {
-            if (i % 2 == 0) {
-                answer = answer.subList(0, query[i] + 1);
-
-            } else {
-                answer = answer.subList(query[i], answer.size());
+    public int solution(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            if (arr1.length > arr2.length) {
+                return 1;
             }
+            return -1;
         }
-        return answer;
+        int arr1sum = 0;
+        int arr2sum = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            arr1sum += arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            arr2sum += arr2[i];
+        }
+        if (arr1sum == arr2sum) {
+            return 0;
+        } else {
+            if (arr1sum > arr2sum) {
+                return 1;
+            }
+            return -1;
+        }
+
     }
 }
 
@@ -26,6 +36,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{0, 1, 2, 3, 4, 5}, new int[]{2, 1, 2}));
+        System.out.println(solution.solution(new int[]{49, 13}, new int[]{70, 11, 2}));
     }
 }
