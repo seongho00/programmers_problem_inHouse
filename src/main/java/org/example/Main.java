dcc4a79387b96ad0453c50daa618cd305ea88bb2
seongho00 <tjdwns000927@gmail.com>
2025-05-17 15:31:43 +0900

    뒤에서 5등 위로

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,10 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int[] num_list) {
-        List<Integer> answer = new ArrayList<>();
+    public int[] solution(int[] num_list) {
         Arrays.sort(num_list);
-        for (int i = 0; i < 5; i++) {
-            answer.add(num_list[i]);
-        }
-        return answer;
+        return Arrays.copyOfRange(num_list, 5, num_list.length);
+
     }
 }
 
@@ -19,6 +16,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{12, 4, 15, 46, 38, 1, 14}));
+        System.out.println(solution.solution(new int[]{12, 4, 15, 46, 38, 1, 14, 56, 32, 10}));
     }
 }
