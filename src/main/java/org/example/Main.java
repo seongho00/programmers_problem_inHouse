8223757bcf84fd76e2674be9de76aa4a71b41d57
seongho00 <tjdwns000927@gmail.com>
2025-05-17 15:54:16 +0900

    문자열을 정수로 변환하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,8 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 class Solution {
-    public int solution(String num_str) {
-        int answer = 0;
-        List<String> list = new ArrayList<>(Arrays.asList(num_str.split("")));
-        for (int i = 0; i < list.size(); i++) {
-            answer += Integer.parseInt(list.get(i));
-        }
-        return answer;
+    public int solution(String n_str) {
+        return Integer.parseInt(n_str);
     }
 }
 
