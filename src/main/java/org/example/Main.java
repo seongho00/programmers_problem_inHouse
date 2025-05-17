27004b1d01b654d4aa8668e02342cfddd2c5faf0
seongho00 <tjdwns000927@gmail.com>
2025-05-17 15:51:12 +0900

    전국 대회 선발 고사

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,25 @@ import java.sql.SQLOutput;
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] num_list) {
-        Arrays.sort(num_list);
-        return Arrays.copyOfRange(num_list, 5, num_list.length);
-
+    public int solution(int[] rank, boolean[] attendance) {
+        int answer = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < rank.length; i++) {
+            list.add(rank[i]);
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < rank.length; i++) {
+            if (attendance[i]) {
+                map.put(rank[i], list.indexOf(rank[i]));
+            }
+        }
+        List<Integer> result_list = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            result_list.add(map.get(Collections.min(map.keySet())));
+            map.remove(Collections.min(map.keySet()));
+        }
+        answer = result_list.get(0) * 10000 + result_list.get(1) * 100 + result_list.get(2);
+        return answer;
     }
 }
 
@@ -16,6 +31,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{12, 4, 15, 46, 38, 1, 14, 56, 32, 10}));
+        System.out.println(solution.solution(new int[]{3, 7, 2, 5, 4, 6, 1}, new boolean[]{false, true, true, true, true, false, false}));
     }
 }
