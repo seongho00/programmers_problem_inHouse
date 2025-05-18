b20153e9749c2f590cb8b45829975355801b4df9
seongho00 <tjdwns000927@gmail.com>
2025-05-18 12:55:50 +0900

    부분 문자열인지 확인하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,24 +8,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Solution {
-    public List<Integer> solution(int[] arr, int[] delete_list) {
-        List<Integer> answer = new ArrayList<>();
-
-        for (int i = 0; i < arr.length; i++) {
-            boolean isPlus = true;
-            for (int j = 0; j < delete_list.length; j++) {
-                if (arr[i] == delete_list[j]) {
-                    isPlus = false;
-                    break;
-                }
-
-            }
-            if (isPlus) {
-                answer.add(arr[i]);
-            }
-
-        }
-        return answer;
+    public int solution(String my_string, String target) {
+        return my_string.contains(target) ? 1 : 0;
     }
 }
 
@@ -33,6 +17,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{293, 1000, 395, 678, 94}, new int[]{94, 777, 104, 1000, 1, 12}));
+        System.out.println(solution.solution("banana", "ana"));
     }
 }
