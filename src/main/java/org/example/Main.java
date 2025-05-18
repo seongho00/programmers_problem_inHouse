11917fef891aa980c93fc3834a3cb61c509007b8
seongho00 <tjdwns000927@gmail.com>
2025-05-18 12:53:53 +0900

    배열의 원소 삭제하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,31 @@
 package org.example;
 
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.stream.Collectors;
+import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
-    public String solution(int n) {
-        return String.valueOf(n);
+    public List<Integer> solution(int[] arr, int[] delete_list) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            boolean isPlus = true;
+            for (int j = 0; j < delete_list.length; j++) {
+                if (arr[i] == delete_list[j]) {
+                    isPlus = false;
+                    break;
+                }
+
+            }
+            if (isPlus) {
+                answer.add(arr[i]);
+            }
+
+        }
+        return answer;
     }
 }
 
@@ -16,6 +33,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(123));
+        System.out.println(solution.solution(new int[]{293, 1000, 395, 678, 94}, new int[]{94, 777, 104, 1000, 1, 12}));
     }
 }
