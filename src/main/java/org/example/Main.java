0cfb2d35e0a54dab5364cd3931c50cc7a050a57b
seongho00 <tjdwns000927@gmail.com>
2025-05-18 13:26:23 +0900

    날짜 비교하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,14 +8,26 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Solution {
-    public int solution(int a, int b) {
-        int answer = 0;
-        if (a % 2 != 0 && b % 2 != 0) {
-            return a * a + b * b;
-        } else if (a % 2 != 0 || b % 2 != 0) {
-            return 2 * (a + b);
+    public int solution(int[] date1, int[] date2) {
+
+        if (date1[0] < date2[0]) {
+            return 1;
+        } else if (date1[0] > date2[0]) {
+            return 0;
         } else {
-            return Math.abs(a - b);
+            if (date1[1] < date2[1]) {
+                return 1;
+            } else if (date1[1] > date2[1]) {
+                return 0;
+            } else {
+                if (date1[2] < date2[2]) {
+                    return 1;
+                } else if (date1[2] > date2[2]) {
+                    return 0;
+                } else {
+                    return 0;
+                }
+            }
         }
     }
 }
@@ -24,6 +36,7 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(3, 5));
+        System.out.println(solution.solution(new int[]{2021, 12, 28}, new int[]{2021, 12, 29}));
+
     }
 }
