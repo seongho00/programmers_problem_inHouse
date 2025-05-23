fb5f3109d3d1af2a5d4829f97e3dd0aeb7514aa7
seongho00 <tjdwns000927@gmail.com>
2025-05-23 14:30:30 +0900

    정사각형으로 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,15 +8,20 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Solution {
-    public int solution(int[][] arr) {
+    public int[][] solution(int[][] arr) {
+        int length = 0;
+        if (arr.length > arr[0].length) {
+            length = arr.length;
+        } else {
+            length = arr[0].length;
+        }
+        int[][] answer = new int[length][length];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i][j] != arr[j][i]) {
-                    return 0;
-                }
+                answer[i][j] = arr[i][j];
             }
         }
-        return 1;
+        return answer;
     }
 }
 
@@ -24,6 +29,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[][]{{5, 192, 33}, {192, 72, 95}, {33, 95, 999}}));
+        System.out.println(solution.solution(new int[][]{{57, 192, 534, 2}, {9, 345, 192, 999}}));
     }
 }
