b0aebb595a4475ae6ff57a1efe9d072be366e773
seongho00 <tjdwns000927@gmail.com>
2025-05-23 14:01:28 +0900

    특별한 이차원 배열 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,28 +7,20 @@ import java.util.stream.Collectors;
 import java.util.List;
 import java.util.ArrayList;
 
-class Solution {
-    public int solution(int[] date1, int[] date2) {
 
-        if (date1[0] < date2[0]) {
-            return 1;
-        } else if (date1[0] > date2[0]) {
-            return 0;
-        } else {
-            if (date1[1] < date2[1]) {
-                return 1;
-            } else if (date1[1] > date2[1]) {
-                return 0;
-            } else {
-                if (date1[2] < date2[2]) {
-                    return 1;
-                } else if (date1[2] > date2[2]) {
-                    return 0;
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    answer[i][j] = 1;
                 } else {
-                    return 0;
+                    answer[i][j] = 0;
                 }
             }
         }
+        return answer;
     }
 }
 
@@ -36,7 +28,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.solution(new int[]{2021, 12, 28}, new int[]{2021, 12, 29}));
-
+        System.out.println(solution.solution(3));
     }
 }
